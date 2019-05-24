41a7b4dae86dd713c517ad2f31e6b74847811eee
york <ytx1988320@163.com>
2019-05-24 17:48:56 +0800

    zinx 学习

--- znet/msghandler.go
+++ znet/msghandler.go
@@ -2,12 +2,17 @@ package znet
 
 import (
 	"fmt"
+	"learn_zinx/util"
 	"learn_zinx/ziface"
 	"strconv"
 )
 
 type MsgHandler struct {
 	Apis map[uint32]ziface.IRouter
+	//任务队列
+	TaskQueue []chan ziface.IRequest
+	//任务池数量大小
+	TaskPoolSize uint32
 }
 
 func (m *MsgHandler) DoMsgHandler(request ziface.IRequest) {
@@ -33,7 +38,51 @@ func (m *MsgHandler) AddRouter(msgID uint32, router ziface.IRouter) {
 
 func NewMsgHandler() (handler *MsgHandler) {
 	handler = &MsgHandler{
-		Apis: make(map[uint32]ziface.IRouter),
+		Apis:         make(map[uint32]ziface.IRouter),
+		TaskQueue:    make([]chan ziface.IRequest, util.Global.TaskPoolSize),
+		TaskPoolSize: util.Global.TaskPoolSize,
 	}
 	return
+}
+
+//初始化任务池
+func (m *MsgHandler) InitTaskPool() {
+
+	//初始化任务队列
+	//根据TaskPoolSize 分别开启TaskQueue池，每个TaskQueue用一个go来承载
+	for i := 0; i < int(util.Global.TaskPoolSize); i++ {
+		// 1 当前的TaskQueue对应的channel消息队列 开辟空间 第0个worker 就用第0个channel ...
+		m.TaskQueue[i] = make(chan ziface.IRequest, util.Global.MaxTaskChanLen)
+		//2 启动当前的TaskQueue， 阻塞等待消息从channel传递进来
+		go m.initTaskChan(i, m.TaskQueue[i])
+	}
+
+}
+
+//
+func (m *MsgHandler) initTaskChan(taskId int, taskQueue chan ziface.IRequest) {
+	fmt.Println("task ID = ", taskId, " is started ...")
+	//不断的阻塞等待对应消息队列的消息
+	for {
+		select {
+		case request := <-taskQueue:
+			m.DoMsgHandler(request)
+		}
+	}
+}
+
+//消息放到任务队列中(采用轮训的方式)
+func (m *MsgHandler) PushToTaskQueue(request ziface.IRequest) {
+	//取模，决定放入哪个队列
+	queueId := request.GetConnection().GetConnId() % m.TaskPoolSize
+
+	fmt.Println("Add ConnID = ", request.GetConnection().GetConnId(),
+		" reqeust MsgID = ", request.GetMsgID(),
+		" to queueId = ", queueId)
+
+	m.TaskQueue[queueId] <- request
+}
+
+func (m *MsgHandler) GetTaskPoolSize() uint32 {
+	return m.TaskPoolSize
 }
